332c746e8dd2ff3926c415ddc7d13c7cd8f71a8a
KolesnikovNikolai <k.o.l.y.a.n-kolesnikov@mail.ru>
2019-02-16 23:12:51 +0700

    refs #Demid first commit

--- CreateMem/Form1.Designer.cs
+++ CreateMem/Form1.Designer.cs
@@ -2,6 +2,7 @@
 {
     partial class Form1
     {
+        //todo необходимы переименования форм, кнопок и т.п.
         /// <summary>
         /// Required designer variable.
         /// </summary>
